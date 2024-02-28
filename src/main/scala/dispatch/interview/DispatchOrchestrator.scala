911b6f70aece1c99980c495e8a63dedd74ed2d5b
ait abdesselam rachid <ait.rachid.01@gmail.com>
2024-02-28 18:17:11 +0100

    first commit

--- src/main/scala/dispatch/interview/DispatchOrchestrator.scala
+++ src/main/scala/dispatch/interview/DispatchOrchestrator.scala
@@ -12,6 +12,7 @@ import org.http4s.dsl.io.{->, /, POST, _}
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.{HttpRoutes, Request, Status, Uri}
+import cats.implicits._
 
 case class Contact(email: String)
 object Contact {
@@ -28,30 +29,81 @@ object DispatchPayload {
   given codec: Codec[DispatchPayload] = deriveCodec[DispatchPayload]
 }
 
-class DispatchEndpoints(http: Client[IO], stationApiUrl: String) {
+
+ trait IEmailService {
+  def sendEmail(content: List[Email]):IO[List[String]]
+}
+
+class EmailService(http: Client[IO], stationApiUrl: String) extends IEmailService {
+
+
+  override def sendEmail(content: List[Email]): IO[List[String]] = {
+    content.map { obj =>
+      http.expect[String](Request[IO](POST, Uri.unsafeFromString(s"$stationApiUrl/email")).withEntity(
+        obj))
+    }.sequence
+  }
+}
+class DispatchEndpoints(http: Client[IO], 
+                        stationApiUrl: String,
+                         emailService:IEmailService) {
+
   def service: HttpRoutes[IO] = HttpRoutes.of[IO] {
 
     case req@POST -> Root / "dispatchNow" =>
       req.as[DispatchPayload].flatMap { dp =>
-        if (dp.timeInSeconds < 600) BadRequest("Asset has minimum dispatch of 600s")
-
-        http.run(Request(POST, Uri.unsafeFromString(s"$stationApiUrl/dispatch/${dp.stationId}/${dp.timeInSeconds}"))).use { res =>
-          res.status match {
-            case Status.Ok => Ok(s"acknowledged")
-          }
+        if (dp.timeInSeconds < 60 * 10 ) {
+         BadRequest("Asset has minimum dispatch of 600s")
+        }else if (dp.timeInSeconds > 60 * 120){
+          BadRequest("Asset has max dispatch of 120m")
         }
+        else {
+
+          http.run(Request(POST, Uri.unsafeFromString(s"$stationApiUrl/dispatch/${dp.stationId}/${dp.timeInSeconds}")))
+            .use { res =>
+              res.status match {
+                case Status.Ok => {
+
+                  val emailObjects = dp.notifications.toList.flatMap(notOp => notOp.map{not =>
+                    Email(contact = not, content = "dispach ok")
+                  })
+
+
+
+                  emailService.sendEmail(emailObjects).handleError {
+                    case e:Exception => {
+                      println(e)
+                    }
+
+                  }.flatMap(_ =>    {
+                    Ok(s"acknowledged")
+                  }
+                  )
+                }
+
+                case Status.BadRequest => BadRequest("fail")
+              }
+            }
+
+        }
+
       }
+
   }
 }
 
 object DispatchEndpoints extends IOApp.Simple {
+
+  var emailService:IEmailService = null
   override def run: IO[Unit] =
     (for {
       client <- EmberClientBuilder.default[IO].build
       server <- EmberServerBuilder.default[IO]
         .withHost(Hostname.fromString("127.0.0.1").get)
         .withPort(Port.fromInt(8889).get)
-        .withHttpApp(new DispatchEndpoints(client, "https://eu.httpbin.org/anything").service.orNotFound)
+        .withHttpApp(new DispatchEndpoints(client, "https://eu.httpbin.org/anything",
+          null
+        ).service.orNotFound)
         .build
     } yield server).useForever
 }
--- src/test/scala/test/DispatchOrchestratorSpec.scala
+++ src/test/scala/test/DispatchOrchestratorSpec.scala
@@ -3,7 +3,7 @@ package test
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 import cats.implicits.catsSyntaxOptionId
-import dispatch.interview.{Contact, DispatchEndpoints, DispatchPayload, Email}
+import dispatch.interview.{Contact, DispatchEndpoints, DispatchPayload, Email, IEmailService}
 import org.http4s.circe.CirceEntityCodec.{circeEntityDecoder, circeEntityEncoder}
 import org.http4s.client.Client
 import org.http4s.dsl.io.{->, /, POST, *}
@@ -12,6 +12,16 @@ import org.http4s.{HttpRoutes, Method, Request, Response}
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers.shouldBe
 
+
+class FakeEmailService extends IEmailService {
+
+  var lastEmailRes : List[String] = Nil
+  override def sendEmail(content: List[Email]) = {
+    lastEmailRes = content.map(_ => "ok")
+    IO.pure(content.map(_ => "ok"))
+  }
+
+}
 class DispatchOrchestratorSpec extends AnyFunSpec {
   private def stubClient(stationResponse: IO[Response[IO]]): Client[IO] = Client.fromHttpApp {
     HttpRoutes.of[IO] {
@@ -21,7 +31,11 @@ class DispatchOrchestratorSpec extends AnyFunSpec {
   }
 
   private val ok: IO[Response[IO]] = Ok(s"OK - station dispatched")
-  private val dispatchOk = new DispatchEndpoints(stubClient(ok), "")
+
+  private val fakeEmailService = new FakeEmailService()
+  private val dispatchOk = new DispatchEndpoints(stubClient(ok), "", fakeEmailService)
+  private val dispatchKO = new DispatchEndpoints(stubClient(BadRequest()), "", fakeEmailService)
+
 
   it("should dispatch when > min and < max dispatch time") {
     val request = Request[IO](Method.POST, uri"/dispatchNow").withEntity(DispatchPayload(100, 600))
@@ -41,15 +55,38 @@ class DispatchOrchestratorSpec extends AnyFunSpec {
       .run(request).unsafeRunSync().status shouldBe BadRequest
   }
 
-  it("should email optional contact on dispatch") {
-    val request = Request[IO](Method.POST, uri"/dispatchNow").withEntity(DispatchPayload(100, 700, List(Contact("sowen@kiwipowered.com")).some))
+  it("should return 400 when dipspach fails") {
+    val request = Request[IO](Method.POST, uri"/dispatchNow").withEntity(DispatchPayload(100, 650))
+
+    dispatchKO
+      .service
+      .orNotFound
+      .run(request).unsafeRunSync().status shouldBe BadRequest
+  }
+
+  it("should not allow a dispatch less than max dispatch time (120m)") {
+    val request = Request[IO](Method.POST, uri"/dispatchNow").withEntity(DispatchPayload(100, 120*60 + 1))
 
     dispatchOk
       .service
       .orNotFound
-      .run(request).unsafeRunSync() shouldBe Ok
+      .run(request).unsafeRunSync().status shouldBe BadRequest
+  }
 
-    fail("TODO - verify emails were sent")
+  it("should email optional contact on dispatch") {
+    val request = Request[IO](Method.POST, uri"/dispatchNow")
+      .withEntity(DispatchPayload(100, 700, List(Contact("sowen@kiwipowered.com")).some))
+
+    dispatchOk
+      .service
+      .orNotFound
+      .run(request).unsafeRunSync().status shouldBe Ok
+
+
+    assert(fakeEmailService.lastEmailRes.length === 1)
+    assert(fakeEmailService.lastEmailRes === "ok"::Nil)
+
+
   }
 
 
